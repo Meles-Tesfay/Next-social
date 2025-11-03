f366e2d752f24f52151fb02fa9b56e1f00e90666
Meles-Tesfay <mandatory279@gmail.com>
2025-11-03 21:15:29 +0300

    Install nextjs and tailwind css and Create the first template

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactCompiler: true, 
+
+  experimental: {
+    turbo: false,
+  },
 };
 
 export default nextConfig;
